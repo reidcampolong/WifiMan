ef64f1ad122100d6cb9a57b766e1c84270d86be1
Reid Campolong <reidcampolong@gmail.com>
2019-06-23 21:35:59 -0400

    Changed from sync to local data

--- content.js
+++ content.js
@@ -1,5 +1,6 @@
-chrome.storage.sync.get('url', function (data) {
+chrome.storage.local.get('url', function (data) {
     if(data.url === document.URL) {
+        console.log("activated");
         var userField = document.getElementById("user");
         var passField = document.getElementById("password");
         var submitButton = document.querySelector("input[type=submit]");
