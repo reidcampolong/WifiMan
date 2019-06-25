a772a52c6cc793d4f954008fbceb68657e68d2fc
Reid Campolong <reidcampolong@gmail.com>
2019-06-24 23:03:32 -0400

    Added time based delay to prevent spamming

--- content.js
+++ content.js
@@ -1,18 +1,30 @@
 chrome.storage.local.get('url', function (data) {
-    if(data.url === document.URL) {
-        console.log("activated");
-        var userField = document.getElementById("user");
-        var passField = document.getElementById("password");
-        var submitButton = document.querySelector("input[type=submit]");
-        
-        chrome.storage.sync.get('username', function (data) {
-            userField.value = data.username;
+    if(document.URL.includes(data.url)) {
+
+        chrome.storage.local.get('submitted', function (data) {
+            var lastSubmitted = Date.parse(data.submitted);
+
+            var currentTime = new Date();
+            if (!lastSubmitted || (currentTime - lastSubmitted > 5000)) {
+                var userField = document.getElementById("user");
+                var passField = document.getElementById("password");
+                var submitButton = document.querySelector("input[type=submit]");
+                
+                chrome.storage.local.get('username', function (data) {
+                    userField.value = data.username;
+                });
+                
+                chrome.storage.local.get('password', function (data) {
+                    passField.value = data.password;
+                });
+                
+                if (submitButton) {
+                    chrome.storage.local.set({ submitted: currentTime.toString() }, function () {
+                        console.log('Updated last update time');
+                        submitButton.click();
+                    })
+                }
+            }
         });
-        
-        chrome.storage.sync.get('password', function (data) {
-            passField.value = data.password;
-        });
-        
-        submitButton.click();
     }
 });
